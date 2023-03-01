50c3207191474270e4954ace631c39032e234193
luobin <365189883@qq.com>
2023-03-01 12:03:32 +0800

    修改eslink规则

--- mini/.eslintrc.js
+++ mini/.eslintrc.js
@@ -40,11 +40,11 @@ module.exports = {
     'no-multiple-empty-lines': [1, { max: 0 }], // 空行最多不能超过1行
     'no-multi-spaces': 1, // 不能用多余的空格
     'no-mixed-spaces-and-tabs': [2, false], // 禁止混用tab和空格
-    // 'no-regex-spaces': 2,// 禁止在正则表达式字面量中使用多个空格 /foo bar/
+    'no-regex-spaces': 2,// 禁止在正则表达式字面量中使用多个空格 /foo bar/
     'no-spaced-func': 2, // 函数调用时 函数名与()之间不能有空格
     'no-sparse-arrays': 2, // 禁止稀疏数组， [1,,2]
     'no-trailing-spaces': 1, // 一行结束后面不要有空格
     'no-useless-escape': 0,
-    // 'camelcase': 2,// 强制驼峰法命名
+    'camelcase': 2,// 强制驼峰法命名
   },
 }
